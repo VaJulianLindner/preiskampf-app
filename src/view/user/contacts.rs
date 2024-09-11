f4c21fef2a18df09b4b769550b912c5c9a1cfebb
Julian Lindner <julianlindner1@gmail.com>
2024-09-11 16:20:17 +0200

    add missing files

---
+++ src/view/user/contacts.rs
@@ -0,0 +1,14 @@
+use askama::Template;
+use crate::core::context::Context;
+use crate::core::request_extension::HttpExt;
+use crate::view::misc::NotificationTemplate;
+use crate::model::user::User;
+
+#[derive(Template)]
+#[template(path = "views/user/contacts.html")]
+pub struct ContactPageTemplate<'a> {
+    pub authenticated_user: &'a Option<User>,
+    pub notification: Option<NotificationTemplate<'a>>,
+    pub errors: &'a Option<Vec<String>>,
+    pub context: Context<'a>,
+}
