230a0f5a552cb2b3d711df3bd5ff18ef59e47de6
Julian Lindner <julianlindner1@gmail.com>
2024-10-25 20:54:52 +0200

    remove debug code

--- src/model/shopping_list/mod.rs
+++ src/model/shopping_list/mod.rs
@@ -84,15 +84,3 @@ impl<'r> FromRow<'r, PgRow> for ShoppingListItem {
         })
     }
 }
-
-pub trait VecExt {
-    fn to_hashset(&self) {
-
-    }
-}
-
-impl VecExt for Vec<ShoppingListItem> {
-    fn to_hashset(&self) {
-
-    }
-}
